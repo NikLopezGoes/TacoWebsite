653e4bba890dc407915f67c1c622e6e56892207f
NikLopezGoes <niklopez4102@gmail.com>
2025-03-13 01:04:09 -0500

    creating react proj

---
+++ taco-truck-website/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import AboutUs from './components/AboutUs';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <AboutUs />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

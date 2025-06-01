a3c20b5af68498ada8413e6a2647fe3a679ed1bd
NikLopezGoes <niklopez4102@gmail.com>
2025-06-01 15:06:59 -0500

    revamped homepage and adding footer

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/footer.css'; // or a separate CSS file if needed
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+      <footer className="footer">
+        <p>Hello Gamers</p>
+      </footer>
+  );
+}
+
+export default Footer;
